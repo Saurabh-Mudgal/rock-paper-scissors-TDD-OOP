966722d4089e4210039e29e0d89eb7722462bc13
saurabh-mudgal <smsaurabhmudgal@gmail.com>
2021-01-27 18:01:55 +0800

    TDD for .clearLog() successful! Test passed and code refactored for readability and extensibility.

--- test-site/scripts/button-logic.js
+++ test-site/scripts/button-logic.js
@@ -3,4 +3,17 @@ export default class ButtonLogic {
     scrollToTop() {
         window.scrollTo({ top: 0, behavior: 'smooth' });
     }
+
+    clearLog() {
+        const logDivs = document.querySelectorAll(".selection")
+        const records = document.querySelectorAll(".record")
+
+        logDivs.forEach(logDiv => {
+            logDiv.remove()
+        })
+
+        records.forEach(record => {
+            record.innerText = "0"
+        })
+    }
 }
--- tests/button-logic.test.js
+++ tests/button-logic.test.js
@@ -5,6 +5,9 @@
  import ButtonLogic from '../test-site/scripts/button-logic.js';
 
  const buttonLogic = new ButtonLogic();
+ const scrollToTopSpy = jest.spyOn(buttonLogic, "scrollToTop");
+ const clearLogSpy = jest.spyOn(buttonLogic, "clearLog");
+
 
 
 
@@ -37,6 +40,39 @@ describe('class ButtonLogic has correct logic for scrolling and resetting', () =
                     expect(typeof buttonLogic.scrollToTop).toBe("function");
                 })
             })
+
+            it('should scroll to top without errors', () => {
+                const result = buttonLogic.scrollToTop();
+
+                expect(result).toBeUndefined;
+                expect(scrollToTopSpy).toHaveBeenCalledTimes(1);
+        
+                scrollToTopSpy.mockClear();    
+            })
+        })
+
+        describe('.clearLog method exists and executes as intended', () => {
+
+            describe('.clearLog smoke tests', () => {
+
+                it('should exist', () => {
+                    expect(buttonLogic.clearLog).toBeDefined();
+                })
+        
+                it('should be a function type', () => {
+                    expect(typeof buttonLogic.clearLog).toBe("function");
+                })
+            })
+
+            it('should clear log without errors', () => {
+                const result = buttonLogic.clearLog();
+
+                expect(result).toBeUndefined;
+                expect(clearLogSpy).toHaveBeenCalledTimes(1);
+        
+                clearLogSpy.mockClear();    
+            })
+
         })
     })
 })
